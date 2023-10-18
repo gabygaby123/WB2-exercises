cc79c8ef0b4f1f8744145e135ee1d18a01cfd6de
gabygaby123 <108373391+gabygaby123@users.noreply.github.com>
2023-10-18 15:23:17 -0700

    conversion

---
+++ Math.Scripts/numericConversions/conversion_tests.js
@@ -0,0 +1,39 @@
+ // Description:  This script tests various numeric 
+//               conversion techniques 
+// Author: Gabriela Newprogrammer 
+
+var a = "  101.1   "; 
+var b = "55"; 
+var c = "402 Stevens"; 
+var d = "Number 5  "; 
+
+console.log("parseInt a) " + parseInt(a));
+console.log("parseInt a) " + parseInt(b));
+console.log("parseInt a) " + parseInt(c));
+console.log("parseInt a) " + parseInt(d));
+
+console.log();
+console.log("parseFloat a) " + parseFloat(a));
+console.log("parseFloat b) " + parseFloat(b));
+console.log("parseFloat c) " + parseFloat(c));
+console.log("parseFloat d) " + parseFloat(d));
+
+console.log();
+console.log("Number a) " + Number(a));
+console.log("Number b) " + Number(b));
+console.log("Number c) " + Number(c));
+console.log("Number d) " + Number(d));
+
+console.log();
+console.log("Number a) " + Number(a));
+console.log("Number a) " + Number(a));
+console.log("Number a) " + Number(a));
+console.log("Number a) " + Number(a));
+
+console.log();
+console.log("+a) " + +a);
+console.log("+a) " + +b);
+console.log("+a) " + +c);
+console.log("+a) " + +d);
+
+
